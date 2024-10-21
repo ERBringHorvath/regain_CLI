2747f0c8aad8908f9a5e7695154340081d67bad6
Eli Bring Horvath <97261650+ERBringHorvath@users.noreply.github.com>
2024-10-21 09:19:43 -0600

    Delete bin directory

--- bin/bayesnetwork.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-##bayesnetwork.sh
-
-# Get directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-##Run Rscript
-Rscript "$DIR/bayesnetwork.r" "$@"
--- bin/mva.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#MVA
-
-# Get the directory where the script is located
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-Rscript "$DIR/MVA.r" "$@"
